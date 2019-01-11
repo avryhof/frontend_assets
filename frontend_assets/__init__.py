f84817ca621a5203384dce4b1d5b733058f216c8
avryhof <amos@vryhofresearch.com>
2019-01-11 14:45:27 -0500

    Fix setup.py to include assets

--- frontend_assets/__init__.py
+++ frontend_assets/__init__.py
@@ -1,5 +1,5 @@
 """
 @copyright Copyright Amos Vryhof
 """
-__version__ = '0.1'
+__version__ = '0.4'
 __name__ = 'frontend_assets'
--- setup.py
+++ setup.py
@@ -45,14 +45,13 @@ setup(
     author_email=author_email,
     description=description,
     long_description=read('README.md'),
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2'
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
         "django",
